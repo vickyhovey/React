080d5ee0b7a019e28a5c35f3038a8a182ac0965d
vickyhovey <vickyhovey@gmail.com>
2018-05-20 22:20:46 -0400

    arrowfunction2

--- indecision-app/src/playground/es6-arrow-function2.js
+++ indecision-app/src/playground/es6-arrow-function2.js
@@ -1,22 +1,18 @@
-'use strict';
-
 // argument object - no long bound with arrow functions
 
-var add = function add(a, b) {
+const add = (a, b) => {
     //console.log(arguments);
     return a + b;
 };
 console.log(add(55, 1, 1001));
 // this keyword - no longer bound
 
-var user = {
+const user = {
     name: 'Jing',
     cities: ['NYC', 'DC', 'Dublin'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        var cityMessages = this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city;
+    printPlacesLived() {
+        const cityMessages = this.cities.map((city) => {
+            return this.name + ' has lived in ' + city;
         });
         return cityMessages;
         //const that =this;
