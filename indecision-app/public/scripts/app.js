6331d84095db2eae0f2436e0b1d1fdf2cf9e0cb6
vickyhovey <vickyhovey@gmail.com>
2018-05-20 21:30:30 -0400

    arrowfunction

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,6 +1,28 @@
-"use strict";
+'use strict';
 
-var square = function square(x) {
-    return x * x;
+// const square = function (x) {
+//     return x * x;
+// };
+// console.log(square(3));
+// const squareArrow = (x) => {
+//     return x * x;
+// };
+// const squareArrow = (x) => x * x;
+
+// console.log(squareArrow(4));
+
+// Challenge - Use arrow functions
+// getFirstName('Mike Smith') -> "Mike"
+// Create regular arrow function
+// Create arrow function using shorthand syntax
+
+
+// const getFirstName = (name) => {
+//     return name.split(' ')[0];
+// };
+
+var getFirstName = function getFirstName(name) {
+  return name.split(' ')[0];
 };
-console.log(square(8));
+
+console.log(getFirstName('Mike Smith'));
--- indecision-app/src/playground/es6-arrow-function.js
+++ indecision-app/src/playground/es6-arrow-function.js
@@ -1,4 +1,24 @@
-const square = function (x) {
-    return x*x;
-};
-console.log(square(8));
+// const square = function (x) {
+//     return x * x;
+// };
+// console.log(square(3));
+// const squareArrow = (x) => {
+//     return x * x;
+// };
+// const squareArrow = (x) => x * x;
+
+// console.log(squareArrow(4));
+
+// Challenge - Use arrow functions
+// getFirstName('Mike Smith') -> "Mike"
+// Create regular arrow function
+// Create arrow function using shorthand syntax
+
+
+// const getFirstName = (name) => {
+//     return name.split(' ')[0];
+// };
+
+const getFirstName = (name) => name.split(' ')[0];
+
+console.log(getFirstName('Mike Smith'));
