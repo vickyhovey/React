f12e3929eeb1130c84ab2178329e38788d6c3b69
vickyhovey <vickyhovey@gmail.com>
2018-05-27 22:04:52 -0400

    Options_component

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -20,12 +20,15 @@ var IndecisionApp = function (_React$Component) {
     _createClass(IndecisionApp, [{
         key: 'render',
         value: function render() {
+            var title = 'Indecision';
+            var subtitle = 'Put your life in the hands of a computer';
+            var options = ['Thing one', 'Thing two', 'Thing four'];
             return React.createElement(
                 'div',
                 null,
-                React.createElement(Header, null),
+                React.createElement(Header, { title: title, subtitle: subtitle }),
                 React.createElement(Action, null),
-                React.createElement(Options, null),
+                React.createElement(Options, { options: options }),
                 React.createElement(AddOption, null)
             );
         }
@@ -46,18 +49,19 @@ var Header = function (_React$Component2) {
     _createClass(Header, [{
         key: 'render',
         value: function render() {
+
             return React.createElement(
                 'div',
                 null,
                 React.createElement(
                     'h1',
                     null,
-                    'Indecision'
+                    this.props.title
                 ),
                 React.createElement(
                     'h2',
                     null,
-                    'Put your life in the hands of a computer'
+                    this.props.subtitle
                 )
             );
         }
@@ -110,8 +114,12 @@ var Options = function (_React$Component4) {
             return React.createElement(
                 'div',
                 null,
-                'Options component here',
-                React.createElement(Option, null)
+                this.props.options.length,
+
+                //this.props.options.map((option) => <p key={option}>{option}</p>
+                this.props.options.map(function (option) {
+                    return React.createElement(Option, { key: option, optionText: option });
+                })
             );
         }
     }]);
@@ -134,7 +142,8 @@ var Option = function (_React$Component5) {
             return React.createElement(
                 'div',
                 null,
-                'Option component here'
+                'Option: ',
+                this.props.optionText
             );
         }
     }]);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,10 +1,13 @@
 class IndecisionApp extends React.Component {
     render() {
+        const title = 'Indecision';
+        const subtitle = 'Put your life in the hands of a computer'
+        const options = ['Thing one', 'Thing two', 'Thing four'];
         return (
             <div>
-                <Header />
+                <Header title={title} subtitle={subtitle}/>
                 <Action />
-                <Options />
+                <Options options={options}/>
                 <AddOption />
             </div>
         );
@@ -12,10 +15,11 @@ class IndecisionApp extends React.Component {
 }
 class Header extends React.Component {
     render() {
+        
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>Put your life in the hands of a computer</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         );
     }
@@ -34,8 +38,11 @@ class Action extends React.Component {
 class Options extends React.Component {
     render() {
         return <div>
-            Options component here
-            <Option />
+            {this.props.options.length}
+            {
+                //this.props.options.map((option) => <p key={option}>{option}</p>
+                this.props.options.map((option) => <Option key={option} optionText={option}/>)
+            }
           </div>;
     }
 }
@@ -44,7 +51,7 @@ class Option extends React.Component {
     render() {
         return (
             <div>
-                Option component here
+                Option: {this.props.optionText}
             </div>
         );
     }
